683ebf4ff4d5f58920b15abf84e088395c400186
jt851993 <jt851993@gmail.com>
2018-04-27 11:12:40 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+require('dotenv').config({silent: true});
+
+var readline = require('readline-sync');
+var PersonalityInsightsV3 = require('watson-developer-cloud/personality-insights/v3');
+var personality_insights = new PersonalityInsightsV3({
+  username: process.env.INSIGHT_USERNAME,
+  password: process.env.INSIGHT_PASSWORD,
+  version_date: '2017-10-13'
+});
+
+var name = readline.question("Enter your Paragraph:\n");
+
+var params = {
+  // Get the content from the JSON file.
+  content: name,
+  content_type: 'text/plain',
+  consumption_preferences: true,
+  raw_scores: true
+};
+
+
+personality_insights.profile(params, function(error, response) {
+  if (error)
+    console.log('Error:', error);
+  else
+    console.log(JSON.stringify(response, null, 2));
+  }
+);
+
+console.log("Hi " + name + ", nice to meet you.");
